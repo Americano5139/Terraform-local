d8c7d4777ffae274f64b6b97ee4307bb89610f6e
Americano5139 <kimjemomaeng@gmail.com>
2025-06-25 02:15:00 +0000

    Add modular

---
+++ eks-terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "cluster_name" {
+  default = "eks-cluster"
+}
